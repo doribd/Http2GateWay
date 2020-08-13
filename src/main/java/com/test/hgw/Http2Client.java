5fc5b2eb940375f5073060658f8b60bedfbb5a89
Dor Ben Dov <doribd@gmail.com>
2020-08-13 10:41:16 +0300

    adding some errors
    later to be checked by AWS CodeGuru

--- src/main/java/com/test/hgw/Http2Client.java
+++ src/main/java/com/test/hgw/Http2Client.java
@@ -13,12 +13,18 @@ import reactor.core.publisher.Mono;
 public class Http2Client {
     private WebClient client = WebClient.create("https://localhost:8443");
 
+    enum Color { 
+        Blue, Red, Black
+    }
+    
     private Mono<ClientResponse> result = client.get()
             .uri("/test")
             .accept(MediaType.TEXT_PLAIN)
             .exchange();
 
     public String getResult() {
+        Color myColor = Color.Blue;
+        a () -> System.out.println("Hello World! my Color ="+myColor);
         return ">> result = " + result.flatMap(res -> res.bodyToMono(String.class)).block();
     }
 }
